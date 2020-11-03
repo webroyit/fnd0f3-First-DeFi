c8773bd53db796ab17191330aac80ba6dd6cae53
royweb <xytank@gmail.com>
2020-11-03 14:11:18 -0500

    Add a constructor to access other contracts

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -1,6 +1,17 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+import './DappToken.sol';
+import './DaiToken.sol';
+
 contract TokenFarm {
     // pubic is a visibility that allow this value to be access from outside the contract
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    // Access these contracts
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
