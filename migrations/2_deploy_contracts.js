f408e046d093ea9628c63f03cc4b0399a9f512c8
royweb <xytank@gmail.com>
2020-11-03 15:25:11 -0500

    Update migration file to deploy all contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
 // Put new contracts on the blockchain
 // Blockchain is like a database
 
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function (deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function (deployer, network, accounts) {
+  // Deploy Fake DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  // Deploy Token Farm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
 };
