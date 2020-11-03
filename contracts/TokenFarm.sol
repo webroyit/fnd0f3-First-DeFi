4382b1f3eb47e78ae3cf54823c23fb0c613a85a6
royweb <xytank@gmail.com>
2020-11-03 13:19:16 -0500

    Set up TokenFarm Contract

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract TokenFarm {
+    // pubic is a visibility that allow this value to be access from outside the contract
+    string public name = "Dapp Token Farm";
+}
