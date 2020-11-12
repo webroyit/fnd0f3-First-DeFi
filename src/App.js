05b9d39c6ba40b0cba0090e38e89323c0fc411e6
royweb <xytank@gmail.com>
2020-11-12 15:00:34 -0500

    Install web3 and use it to connect to the blockchain

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
 import './App.css';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container-fluid mt-5">
-        <h1>First DeFi</h1>
+class App extends Component{
+  async componentWillMount(){
+    await this.loadWeb3();
+  }
+
+  // Connect the app to the blockchain
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container-fluid mt-5">
+          <h1>First DeFi</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
