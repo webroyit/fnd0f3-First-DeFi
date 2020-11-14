c3f3be2918613d310a7a304ce56c18775e1ec173
royweb <xytank@gmail.com>
2020-11-14 12:54:43 -0500

    Add a form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import dai from '../dai.png';
+
 class Main extends Component{
 
   render(){
     return (
       <div id="content" className="mt-3 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+
         <table className="table table-borderless text-muted text-center">
           <thead>
             <tr>
@@ -19,6 +22,33 @@ class Main extends Component{
             </tr>
           </tbody>
         </table>
+
+        <div className="card mb-4">
+          <div className="card-body">
+            <form className="mb-3">
+              <div>
+                <label className="float-left"><b>Stake Tokens</b></label>
+                <span className="float-right text-muted">
+                  Balance: {window.web3.utils.fromWei(this.props.daiTokenBalance, 'Ether')}
+                </span>
+              </div>
+              <div className="input-group mb-4">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="0"
+                  required />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <img src={dai} height='32' alt="DAI Token"/>
+                    &nbsp;&nbsp;&nbsp; fDAI
+                  </div>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary btn-block btn-lg">STAKE</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
